955d4667164a375e65306e41168a04c5c8377b85
Pedro Lopes <pedrolopesbooking@gmail.com>
2026-02-25 15:07:23 -0300

    please, work

--- backend/jest.config.cjs
+++ backend/jest.config.cjs
@@ -4,10 +4,7 @@ module.exports = {
   testEnvironment: 'node',
   extensionsToTreatAsEsm: ['.ts'],
   moduleNameMapper: {
-    'generated/prisma/index\\.js$': '<rootDir>/generated/prisma/index.js',
-    'generated/prisma/client\\.js$': '<rootDir>/generated/prisma/client.js',
-    
-    '^(\\.{1,2}/.*)\\.js$': '$1',
+    '^(\\.{1,2}/(?!.*generated).*)\\.js$': '$1',
   },
   transform: {
     '^.+\\.tsx?$': [
